d6223e460f9e8f62d9e046f55150b4ca73217c5d
ZhangZhishan <zhangzhishanlo@gmail.com>
2016-03-23 19:15:01 +0800

    Initial Commit

---
+++ momo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  momo
+//
+//  Created by Zhishan Zhang on 3/23/16.
+//  Copyright © 2016 Zhishan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
